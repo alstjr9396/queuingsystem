adf18ceab0e58675fd971d8350a7d69c9a0efc29
kang <minseokkang95@gmail.com>
2024-10-29 10:13:30 +0900

    대기열 스케쥴러 추가

--- queueing/src/main/java/me/minseok/queueing/service/MemberQueueService.java
+++ queueing/src/main/java/me/minseok/queueing/service/MemberQueueService.java
@@ -4,10 +4,16 @@ import static me.minseok.queueing.exception.ErrorCode.QUEUE_ALREADY_REGISTER_MEM
 
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberQueueService {
@@ -15,7 +21,11 @@ public class MemberQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY = "members:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "members:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "members:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
     public Mono<Long> registerWaitQueue(final String queue, final Long memberId) {
         long epochSecond = Instant.now().getEpochSecond();
@@ -43,5 +53,26 @@ public class MemberQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), memberId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void schedulerAllowMember() {
+        if (!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
+        log.info("called scheduling...");
+
+        var maxAllowMemberCount = 3L;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowMember(queue, maxAllowMemberCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowMemberCount, tuple.getT2(), tuple.getT1())))
+                .subscribe();
+
     }
 }
